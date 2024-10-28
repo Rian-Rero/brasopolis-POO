a3045523fd8ca7e79907f1f0c4b155f28432dee9
Victor Rezende <victormrezende3@gmail.com>
2024-10-28 11:23:13 -0300

    fix: class cards

--- src/includes/Cards.py
+++ src/includes/Cards.py
@@ -2,3 +2,9 @@ class Cart:
     def __init__(self, description, action):
         self.description=description
         self.action=action
+
+    def executeAction(self, player):
+        print(f"Executando ação da carta: {self.description}")
+        self.action(player)
+
+#colocar abaixo subclasses (usar herança) de cada tipo de carta especial (definir cartas especiais)
