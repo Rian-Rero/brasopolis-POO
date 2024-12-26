308caced39a84bd8de3586ce7a63ae21fa435e17
Rian-Rero <rian@dharma.com.br>
2024-12-26 17:52:07 -0300

    feat new board

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+def main():
+    run = True
+
+    while run:
+
+     
+
+
+main()
--- src/includes/Dice.py
+++ src/includes/Dice.py
@@ -1,12 +1,6 @@
 import random
 
+
 class Dice:
     def roll(self):
-        return random.randint(1, 6)
-        
-
-def main():
-    dado=Dice()
-    resultado=dado.roll()
-    print("Resultado do lançamento dos dados:", resultado)
-main()
+        return random.randint(1, 12)
