38de1b3dc4b48284e1ded4fa96a82004229898c0
Victor Rezende <victormrezende3@gmail.com>
2024-12-28 18:56:49 -0300

    fix: config. especial cards lose/gain money

--- src/includes/Cards.py
+++ src/includes/Cards.py
@@ -1,10 +1,27 @@
-class Cart:
-    def __init__(self, description, action):
-        self.description=description
-        self.action=action
+from abc import ABC, abstractmethod
+
+class Cart(ABC):
+    def __init__(self, description):
+        self.description = description
+
+    @abstractmethod
+    def executeAction(self, player):
+        pass
+
+class GainMoney(Cart):
+    def __init__(self, description, amount):
+        super().__init__(description)
+        self.amount = 500000
 
     def executeAction(self, player):
         print(f"Executando ação da carta: {self.description}")
-        self.action(player)
+        player.gain_money(self.amount)
 
-#colocar abaixo subclasses (usar herança) de cada tipo de carta especial (definir cartas especiais)
+class LoseMoney(Cart):
+    def __init__(self, description, amount):
+        super().__init__(description)
+        self.amount = 300000
+
+    def executeAction(self, player):
+        print(f"Executando ação da carta: {self.description}")
+        player.lose_money(self.amount)
